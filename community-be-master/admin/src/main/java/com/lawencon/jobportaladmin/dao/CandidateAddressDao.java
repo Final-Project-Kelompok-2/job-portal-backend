27c5df04f03f768b73d9f41ad054281f9bd0081f
santo matthew <matthewalesandro78@gmail.com>
2023-08-10 00:11:25 +0700

    copy from candidates db to admin db on the same files

--- community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/dao/CandidateAddressDao.java
+++ community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/dao/CandidateAddressDao.java
@@ -1,10 +1,78 @@
 package com.lawencon.jobportaladmin.dao;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.base.ConnHandler;
+import com.lawencon.jobportaladmin.model.CandidateAddress;
+import com.lawencon.jobportaladmin.model.CandidateProfile;
+import com.lawencon.jobportaladmin.model.CandidateUser;
 
 @Repository
 public class CandidateAddressDao extends AbstractJpaDao{
+	
+	private EntityManager em() {
+		return ConnHandler.getManager();
+	}
+	
+	public CandidateAddress getByCandidateId(String id){
+		final String sql = "SELECT  "
+				+ "	tca.id, "
+				+ "	address, "
+				+ "	residence_type, "
+				+ "	country, "
+				+ "	province, "
+				+ "	city, "
+				+ "	postal_code, "
+				+ "	user_id,"
+				+ " tcp.fullname "
+				+ "FROM  "
+				+ "	t_candidate_address tca  "
+				+ "INNER JOIN  "
+				+ "	t_candidate_user tcu  "
+				+ "ON  "
+				+ "	tcu.id = tca.user_id  "
+				+ "INNER JOIN "
+				+ "	t_candidate_profile tcp  "
+				+ "ON "
+				+ "	tcp.id = tcu.profile_id  "
+				+ "WHERE  "
+				+ "	tca.user_id  = :candidate";
+		
+		final Object candidateAddressObjs = em().createNativeQuery(sql)
+				.setParameter("candidate", id)
+				.getSingleResult();
+		
+		final Object[] candidateAddressArr = (Object[]) candidateAddressObjs;
 
+		CandidateAddress candidateAddress = null;
+		if(candidateAddressArr.length > 0) {
+			candidateAddress = new CandidateAddress();
+			candidateAddress.setId(candidateAddressArr[0].toString());
+			candidateAddress.setAddress(candidateAddressArr[1].toString());
+			candidateAddress.setResidenceType(candidateAddressArr[2].toString());
+			candidateAddress.setCountry(candidateAddressArr[3].toString());
+			candidateAddress.setProvince(candidateAddressArr[4].toString());
+			candidateAddress.setCity(candidateAddressArr[5].toString());
+			candidateAddress.setPostalCode(candidateAddressArr[6].toString());
+			
+			final CandidateProfile candidateProfile = new CandidateProfile();
+			candidateProfile.setFullname(candidateAddressArr[8].toString());
+			
+			final CandidateUser candidateUser = new CandidateUser();
+			candidateUser.setId(candidateAddressArr[7].toString());
+			candidateUser.setCandidateProfile(candidateProfile);
+			candidateAddress.setCandidateUser(candidateUser);
+		}
+		
+		
+		return candidateAddress;
+
+		
+		
+	}
+	
+	
 }
--- community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/dao/CandidateLanguageDao.java
+++ community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/dao/CandidateLanguageDao.java
@@ -1,10 +1,61 @@
 package com.lawencon.jobportaladmin.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.base.ConnHandler;
+import com.lawencon.jobportaladmin.model.CandidateLanguage;
+import com.lawencon.jobportaladmin.model.CandidateUser;
 
 @Repository
 public class CandidateLanguageDao extends AbstractJpaDao{
 
+	private EntityManager em() {
+		return ConnHandler.getManager();
+	}
+	
+	public List<CandidateLanguage>getLanguageByCandidate(String id){
+		final String sql = "SELECT  "
+				+ "	id, "
+				+ "	language_name, "
+				+ "	writing_rate, "
+				+ "	speaking_rate, "
+				+ "	listening_rate, "
+				+ "	user_id "
+				+ "FROM  "
+				+ "	t_candidate_language tcl "
+				+ " WHERE"
+				+ " user_id = :candidate ";
+		final List<?> languageObjs = em().createNativeQuery(sql)
+				.setParameter("candidate", id)
+				.getResultList();
+		final List<CandidateLanguage> candidateLanguageList = new ArrayList<>();
+		if(languageObjs.size() > 0) {
+			for(Object languageObj : languageObjs) {
+				final Object[] languageArr = (Object[]) languageObj;
+				final CandidateLanguage candidateLanguage = new CandidateLanguage();
+				
+				candidateLanguage.setId(languageArr[0].toString());
+				candidateLanguage.setLanguageName(languageArr[1].toString());
+				candidateLanguage.setWritingRate(languageArr[2].toString());
+				candidateLanguage.setSpeakingRate(languageArr[3].toString());
+				candidateLanguage.setListeningRate(languageArr[4].toString());
+				
+				final CandidateUser candidateUser = new CandidateUser();
+				candidateUser.setId(languageArr[5].toString());
+				candidateLanguage.setCandidateUser(candidateUser);
+				
+				candidateLanguageList.add(candidateLanguage);
+				
+			}
+		}
+		return candidateLanguageList;
+		
+	}
+	
 }
