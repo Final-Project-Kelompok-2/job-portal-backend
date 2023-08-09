9ea4c17dd868d55fce9dd12372b9bd5be68df8a9
Eril <m.erilfario19@gmail.com>
2023-08-09 10:45:56 +0700

    model revisi

--- community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/model/Company.java
+++ community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/model/Company.java
@@ -1,7 +1,32 @@
 package com.lawencon.jobportaladmin.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_company")
 public class Company {
 	
+	@Column(name ="company_code", length = 5, nullable = false)
+	private String companyCode;
 	
+	@Column(name = "company_name",length = 30, nullable = false)
+	private String companyName;
+	
+	@Column(name = "address", nullable = false)
+	private String address;
+	
+	@Column(name = "company_url")
+	private String companyUrl;
+	
+	@Column(name = "company_phone",length = 15, nullable = false)
+	private String companyPhone;
+	
+	@OneToOne
+	@JoinColumn(name = "photo_id")
+	private File photo;
 
 }
--- community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/model/FileType.java
+++ community-be-master/admin/src/main/java/com/lawencon/jobportaladmin/model/FileType.java
@@ -1,5 +1,17 @@
 package com.lawencon.jobportaladmin.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_file_type")
 public class FileType {
+	
+	@Column(name = "type_code",length = 5, nullable = false)
+	private String typeCode;
+	
+	@Column(name = "type_name",length = 20, nullable = false)
+	private String typeName;
 
 }
