a45a04bba102be7040eb660f535cd0cb37c8e08b
santo matthew <matthewalesandro78@gmail.com>
2023-08-30 14:50:11 +0700

    modifying download report method on report service

--- admin/src/main/java/com/lawencon/jobportaladmin/service/ReportService.java
+++ admin/src/main/java/com/lawencon/jobportaladmin/service/ReportService.java
@@ -1,15 +1,25 @@
 package com.lawencon.jobportaladmin.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lawencon.jobportaladmin.dto.report.ReportResDto;
+import com.lawencon.util.JasperUtil;
 
 @Service
 public class ReportService {
 
-	public byte[] downloadReport() {
-		//TODO : Ngoding disini ya dit
-		
-		return null;
-		
+	@Autowired
+	private JasperUtil jasperUtil;
+
+	public byte[] downloadReport(List<ReportResDto> reportDatas) throws Exception {
+
+		return  jasperUtil.responseToByteArray
+				(reportDatas, null, "Report");		
 	}
-	
+
 }
