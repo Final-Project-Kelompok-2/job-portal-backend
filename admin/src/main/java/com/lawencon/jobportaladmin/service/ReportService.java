9e736b3c6652698f54e5702c76ac42ca26001f01
Adhitya <adhityaputraminardi@gmail.com>
2023-08-30 14:52:49 +0700

    Controller dan service report

--- admin/src/main/java/com/lawencon/jobportaladmin/service/ReportService.java
+++ admin/src/main/java/com/lawencon/jobportaladmin/service/ReportService.java
@@ -1,15 +1,29 @@
 package com.lawencon.jobportaladmin.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lawencon.jobportaladmin.dao.ReportDao;
+import com.lawencon.jobportaladmin.dto.report.ReportResDto;
 
 @Service
 public class ReportService {
 
+	@Autowired
+	private ReportDao reportDao;
+	
 	public byte[] downloadReport() {
 		//TODO : Ngoding disini ya dit
 		
 		return null;
 		
+	}
+	
+	public List<ReportResDto> getReport(){
+		final List<ReportResDto> reports = reportDao.getReport();
+		return reports;
 	}
 	
 }
