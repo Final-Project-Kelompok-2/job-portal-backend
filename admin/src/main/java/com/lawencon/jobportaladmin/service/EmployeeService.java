53b47db1b4cd6903d56e6d2abb4ad2fd5a6eab16
drewmaruli <201900190@my.apiu.edu>
2023-09-01 14:24:49 +0700

    Update Assign Job To Candidate (ADMIN)

--- admin/src/main/java/com/lawencon/jobportaladmin/service/EmployeeService.java
+++ admin/src/main/java/com/lawencon/jobportaladmin/service/EmployeeService.java
@@ -9,9 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.base.ConnHandler;
+import com.lawencon.jobportaladmin.constant.PersonTypes;
+import com.lawencon.jobportaladmin.dao.CandidateProfileDao;
+import com.lawencon.jobportaladmin.dao.CandidateUserDao;
 import com.lawencon.jobportaladmin.dao.EmployeeDao;
+import com.lawencon.jobportaladmin.dao.JobDao;
+import com.lawencon.jobportaladmin.dao.PersonTypeDao;
+import com.lawencon.jobportaladmin.dto.InsertResDto;
+import com.lawencon.jobportaladmin.dto.employee.EmployeInsertReqAdminDto;
 import com.lawencon.jobportaladmin.dto.employee.EmployeeResDto;
+import com.lawencon.jobportaladmin.model.CandidateProfile;
+import com.lawencon.jobportaladmin.model.CandidateUser;
 import com.lawencon.jobportaladmin.model.Employee;
+import com.lawencon.jobportaladmin.model.Job;
+import com.lawencon.jobportaladmin.model.PersonType;
+import com.lawencon.jobportaladmin.util.GenerateCode;
 import com.lawencon.security.principal.PrincipalService;
 
 @Service
@@ -23,9 +35,21 @@ public class EmployeeService {
 	
 	@Autowired
 	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private CandidateUserDao candidateUserDao;
+	
+	@Autowired
+	private CandidateProfileDao candidateProfileDao;
+	
+	@Autowired
+	private PersonTypeDao personTypeDao;
+	
+	@Autowired
+	private JobDao jobDao;
+	
 	@Autowired
 	private PrincipalService<String> principalService;
-	
 	
 	public List<EmployeeResDto>getAll(){
 		final List<Employee> getList = employeeDao.getAll(Employee.class);
@@ -43,6 +67,36 @@ public class EmployeeService {
 		return employeeRes;
 	}
 
-	
-	
+	public InsertResDto insertEmployeeFromAdmin(EmployeInsertReqAdminDto data) {
+		final InsertResDto response = new InsertResDto();
+		
+		try {
+			em().getTransaction().begin();
+			final Employee employee = new Employee();
+			final CandidateUser candidateUser = candidateUserDao.getById(CandidateUser.class, data.getCandidateId());
+			final Job job = jobDao.getById(Job.class, data.getJobId());
+			employee.setCandidate(candidateUser);
+			employee.setJob(job);
+			employee.setEmployeeCode(GenerateCode.generateCode());
+			employee.setCreatedBy(principalService.getAuthPrincipal());
+			employeeDao.save(employee);
+			
+			response.setId(employee.getId());
+			response.setMessage("Candidate Assigned to a Job");
+			
+			CandidateProfile candidateProfile = candidateProfileDao.getById(CandidateProfile.class, candidateUser.getCandidateProfile().getId());
+			final PersonType personType = personTypeDao.getByCode(PersonTypes.EMPLOYEE.typeCode);
+			candidateProfile.setPersonType(personType);
+			
+			candidateProfile = candidateProfileDao.saveAndFlush(candidateProfile);
+			
+			em().getTransaction().commit();
+		} catch (Exception e) {
+			em().getTransaction().rollback();
+			e.printStackTrace();
+			throw new RuntimeException("Assign Candidate to a Job Failed");
+		}
+		
+		return response;
+	}
 }
